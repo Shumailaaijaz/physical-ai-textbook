2a3688632aa330f08e2694a4ec3862723ed16e94
shumailaaijaz <shumailaaijaz@users.noreply.github.com>
2025-12-23 02:00:51 +0500

    Trigger fresh deployment with clean build config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -35,6 +35,7 @@ const config: Config = {
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
